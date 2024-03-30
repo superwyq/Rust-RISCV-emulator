af32903d31b62e8589c371a892fa6f3046e3d04a
Wang Yongqi <shuwyq@foxmail.com>
2024-03-30 21:51:17 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+pub mod  cpu;
+fn main() {
+    println!("Hello, world!");
+    let mut cpu = cpu::cpu::CPU::new();
+    let instruction:u32 = 0b00010000000000000000000010010011;
+    cpu.memory.write(0x200, (instruction & 0xFF) as u8);
+    cpu.memory.write(0x201, ((instruction >> 8) & 0xFF) as u8);
+    cpu.memory.write(0x202, ((instruction >> 16) & 0xFF) as u8);
+    cpu.memory.write(0x203, ((instruction >> 24) & 0xFF) as u8);
+
+    cpu.run(true);
+}
